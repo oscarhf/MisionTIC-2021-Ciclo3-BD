12a0b27bae0b9753e86360437db295b56a266391
Profe Oscar <oscar.franco@ucaldas.edu.co>
2021-08-24 22:15:33 +0000

    Initial Commit

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+    class Program
+    {
+        
+        private static IRepositorioPaciente _repoPaciente= new RepositorioPaciente(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World EF!");
+            //AddPaciente();
+            //BuscarPaciente(1);
+            AsignarMedico();
+        }
+
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Nicolay",
+                Apellidos = "Perez",
+                NumeroTelefono = "3001645",
+                Genero = Genero.Masculino,
+                Direccion = "Calle 4 No 7-4",
+                Longitud = 5.07062F,
+                Latitud = -75.52290F,
+                Ciudad = "Manizales",
+                FechaNacimiento = new DateTime(1990, 04, 12)
+            };
+            _repoPaciente.AddPaciente(paciente);
+          
+        }
+
+        private static void BuscarPaciente(int idPaciente)
+        {
+            var paciente = _repoPaciente.GetPaciente(idPaciente);
+            Console.WriteLine(paciente.Nombre+" "+paciente.Apellidos);
+        }
+
+        private static void AsignarMedico()
+        {
+            var medico = _repoPaciente.AsignarMedico(1,2);
+            Console.WriteLine(medico.Nombre+" "+medico.Apellidos);
+        }
+    }
+}
